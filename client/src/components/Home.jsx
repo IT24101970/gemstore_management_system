ebc8b6c4e8c25dfba4ab7e8932aab620154e2a8d
IT24102382 <IT24102382@my.sliit.lk>
2026-04-16 16:25:44 +0530

    ..

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -183,7 +183,7 @@ const Home = ({ user, onLogout }) => {
                                 <div className="home-wallet">
                                     <span className="material-symbols-outlined">account_balance_wallet</span>
                                     <span>${balance.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                                </Link>
+                                </div> 
                                 <button className="home-icon-btn">
                                     <span className="material-symbols-outlined">notifications</span>
                                 </button>
