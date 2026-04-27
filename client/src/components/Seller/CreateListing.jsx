641189e0c816b79a8b3465073a1a0c2cfe08a3d4
Owner <IT24101405@my.sliit.lk>
2026-04-27 21:57:18 +0530

    created view/delete report option on create listing page

--- client/src/components/Seller/CreateListing.jsx
+++ client/src/components/Seller/CreateListing.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../NavBar";
 import "../Home.css";
 import "./CreateListing.css";
